1f553e54af3ec24bdf1e7c707e6f1676e36c2f11
ASUQ <asuq.4096@gmail.com>
2025-10-09 18:45:06 +0200

    fix: extract instrument model to distinguish pacbio rs and hifi

--- bin/filter_sra.sh
+++ bin/filter_sra.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
-# Usage: ./filter_sra.sh SRR123456.metadata.tsv
+# Usage: ./filter_sra.sh ACCESSION.metadata.csv
+# Output: ACCESSION.filtered.csv
 
 in="$1"
 out="${in%.metadata.tsv}.filtered.csv"
 
-# Filters (edit easily)
+# Filters
 PLATFORMS='^(ILLUMINA|BGISEQ|DNBSEQ|PACBIO_SMRT|OXFORD_NANOPORE)$'
 SOURCES='^(GENOMIC|METAGENOMIC)$'
 STRATEGIES='^WGS$'
 
 awk -F'\t' -v OFS=',' -v P="$PLATFORMS" -v S="$SOURCES" -v T="$STRATEGIES" '
 BEGIN {
-  r="run_accession"; p="instrument_platform"; s="library_source"; t="library_strategy"
+  r="run_accession"
+  p="instrument_platform"
+  m="instrument_model"
+  s="library_source"
+  t="library_strategy"
 }
 NR==1 {
   for(i=1;i<=NF;i++){ sub(/\r$/,"",$i); idx[$i]=i }
-  print r, p, s, t; next
+  print r, p, m, s, t
+  next
 }
 {
   sub(/\r$/,"",$0)
   if ($idx[p] ~ P && $idx[s] ~ S && $idx[t] ~ T)
-    print $idx[r], $idx[p], $idx[s], $idx[t]
+    print $idx[r], $idx[p], $idx[m], $idx[s], $idx[t]
 }
 ' "$in" > "$out"
