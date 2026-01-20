6075a0082e15f71355343a84149ea98256af12cf
ASUQ <asuq.4096@gmail.com>
2026-01-20 10:20:19 +0100

    fix: ctrl+c bug

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ INTERVAL_MIN=10
 NF_SRA_SCREEN='/path/to/nf-sra_screen'
 
 WATCH_PID=""
+nf_exit=0
 
 # mamba activate /bucket/HusnikU/Conda-envs/nextflow_v25.04.8
 # module load singularity
@@ -41,7 +42,9 @@ cleanup() {
 #  - normal exit (EXIT)
 #  - Ctrl+C (INT)
 #  - termination (TERM)
-trap cleanup EXIT INT TERM
+trap cleanup EXIT
+trap 'exit 130' INT
+trap 'exit 143' TERM
 
 # ------------------------------------------------------------------
 # Go to run directory (where .nextflow.log, work/, output/ will live)
@@ -64,9 +67,8 @@ echo "watch_and_transfer pid: ${WATCH_PID}"
 printf '%s\n' "${WATCH_PID}" > watch_and_transfer.pid
 
 # ------------------------------------------------------------------
-# Run Nextflow pipeline (do not let set -e hide its exit code)
+# Run Nextflow pipeline
 # ------------------------------------------------------------------
-set +e
 nextflow run asuq/nf-sra_screen \
   -profile <docker/singularity/local/slurm/...> \
   --sra sra.csv \
@@ -79,9 +81,8 @@ nextflow run asuq/nf-sra_screen \
   --sandpiper_db /path/to/sandpiper_db_dir \
   --singlem_db /path/to/singlem_metapackage \
   --outdir nf-sra_screen_results \
-  -resume
-nf_exit=$?
-set -e
+  -resume \
+|| nf_exit=$?
 
 # This exit will trigger the EXIT trap, which runs cleanup()
 exit "${nf_exit}"
